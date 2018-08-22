e10c8d7cd6203cd9059d9486f6f00650dfc0189a
jocelyngiquel <jgiquel@yahoo.com>
2018-08-22 20:59:05 +0200

    Added Foursquare ajax request

--- js/map.js
+++ js/map.js
@@ -13,13 +13,14 @@ function initMap() {
     styles: mapstyles
   });
 
-  var largeInfowindow = new google.maps.InfoWindow();
+  var infoWindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
 
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
     var position = locations[i].location;
     var title = locations[i].title;
+    var fsid = locations[i].fsid;
     var markerIcon = 'img/swiss-army-knife.png';
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
@@ -28,13 +29,14 @@ function initMap() {
       title: title,
       icon: markerIcon,
       animation: google.maps.Animation.DROP,
+      fsid: fsid,
       id: i
     });
     // Push the marker to our array of markers.
     markers.push(marker);
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
+      populateInfoWindow(this, infoWindow);
     });
     bounds.extend(markers[i].position);
   }
@@ -45,8 +47,38 @@ function initMap() {
 function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
+    infowindow.setContent('');
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
+
+    url = "https://api.foursquare.com/v2/venues/";
+    clientID = "?&client_id=ISZVSBC0XU3A0FY0GOXREVFAKP5ID410LUJQJ4LJMHRC4ICB";
+    clientSecret = "&client_secret=YZEU550LVWJVGML2SL0WJDF4ICD2UPVFJM52HO3FPWMSWIGJ";
+    version = "&v=20180822";
+
+    var fsURL = url + marker.fsid + clientID + clientSecret + version;
+
+    $.ajax({
+      url: fsURL,
+      dataType: 'jsonp',
+      success:function(result) {
+        if (result.meta["code"] == 429) {
+          render = '<div>' + '<p>' + 'Foursquare error: ' + result.meta["errorDetail"] + '</p>' + '</div>';
+          infowindow.setContent(window.render);
+        } else {
+        var response = result.response;
+        var shortURL = response.venue["shortUrl"];
+            photoUrl = response.venue.bestPhoto["prefix"] + "height150" + response.venue.bestPhoto["suffix"];
+            rating = response.venue["rating"];
+            ratingColor = response.venue["ratingColor"];
+            render = '<div>' + '<p>' + marker.title + '<br>' + rating + '<br>' + shortURL + '<br>' + ratingColor + '<br>' + photoUrl + '</p>' + '</div>';            
+            infowindow.setContent(window.render2);
+      }},
+      error: function() {
+        render = '<div>' + '<p>' + "Okay, houston, we've had a problem" + '</p>' + '</div>';
+        infowindow.setContent(window.render);
+      }
+
+    });
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick',function(){
--- js/model.js
+++ js/model.js
@@ -1,6 +1,7 @@
 var  locations = [
     {
       title: "Tavolone Bains",
+      fsid: "510c0a89e4b0e2f930af3571",
       location: {
         lat: 46.1992743,
         lng: 6.1372045
@@ -8,16 +9,74 @@ var  locations = [
     },
     {
       title: "La Buvette des bains",
+      fsid: "4af27f52f964a520cde721e3",
       location: {
         lat: 46.210266,
-        lng: 6.1519023
+        lng: 6.1540912
       }
     },
     {
-      title: "L'Escalier",
+      title: "Le Scandale",
+      fsid: "4adcdab1f964a520f34f21e3",
       location: {
-        lat:46.2010704,
-        lng:6.1566466
+        lat:46.2119753,
+        lng:6.1449811
+      }
+    },
+    {
+      title: "Inglewood PlainPalais",
+      fsid: "4eba5c452c5bdfcbf71da77f",
+      location: {
+        lat:46.1941365,
+        lng:6.1402389
+      }
+    },
+    {
+      title: "Cafe du Lys",
+      fsid: "4af49b5df964a52049f421e3",
+      location: {
+        lat:46.1972107,
+        lng:6.1385718
+      }
+    },
+    {
+      title: "Parfums de Beyrouth",
+      fsid: "4b7451aef964a520e8d42de3",
+      location: {
+        lat:46.2099381,
+        lng:6.14596099
+      }
+    },
+    {
+      title: "Brasserie des Halles de L'île",
+      fsid: "4bac8460f964a520dff83ae3",
+      location: {
+        lat:46.2047069,
+        lng:6.1409869
+      }
+    },
+    {
+      title: "Luigia",
+      fsid: "4c76d3ea947ca1cdfe784537",
+      location: {
+        lat:46.1993241,
+        lng:6.1561736
+      }
+    },
+    {
+      title: "Ou Bien Encore",
+      fsid: "55a5354f498e05d4f53536fe",
+      location: {
+        lat:46.2008516,
+        lng:6.1374667
+      }
+    },
+    {
+      title: "Cafe la Clemence",
+      fsid: "4adcdab1f964a520cf4f21e3",
+      location: {
+        lat:46.2002261,
+        lng:6.1487932
       }
     },
   ]
